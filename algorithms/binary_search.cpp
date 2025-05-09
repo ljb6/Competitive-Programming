8134ef60c7869f9361c36768af17c10d07d2b040
Lucca <81322668+ljb6@users.noreply.github.com>
2025-05-09 19:25:45 -0300

    Update binary_search.cpp

--- algorithms/binary_search.cpp
+++ algorithms/binary_search.cpp
@@ -1,19 +1,8 @@
 #include <bits/stdc++.h>
 
-
-#define _ ios_base::sync_with_stdio(0);cin.tie(0);
-#define endl '\n'
-#define f first
-#define s second
-
-typedef long long ll;
-
-const int INF = 0x3f3f3f3f;
-const ll LINF = 0x3f3f3f3f3f3f3f3fll;
-
 using namespace std;
 
-int main() { _
+int main() {
     // arr size
     int n; cin >> n;
     vector<int> vec(n);
