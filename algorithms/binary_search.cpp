be7938788bccbc997fdd364cb272739039f84011
Lucca <81322668+ljb6@users.noreply.github.com>
2025-05-09 19:25:19 -0300

    Add files via upload

---
+++ algorithms/binary_search.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+int main() { _
+    // arr size
+    int n; cin >> n;
+    vector<int> vec(n);
+
+    // target
+    int t; cin >> t;
+
+    // adding elements * already ordered
+    for (int i = 0; i < n; i++) cin >> vec[i];
+
+    // binary search
+    int l = 0, r = n - 1;
+    bool found = false;
+    while (l <= r) {
+        int mid = (l+r)/2;
+        if (vec[mid] == t) {
+            cout << "Found at pos: " << mid << endl;
+            found = true;
+            break;
+        } else {
+            if (vec[mid] < t) l = mid + 1;
+            else r = mid - 1;
+        }
+    }
+    if (!found) cout << "Target not found" << endl;
+
+    return 0;
+}
+
