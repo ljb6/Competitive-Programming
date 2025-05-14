8173035a043fbdd34730398d514860c7c4ba2ccf
Lucca <81322668+ljb6@users.noreply.github.com>
2025-05-14 15:45:03 -0300

    Add files via upload

---
+++ CODE@NIGHT 2024/Mergulho.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+
+#define _ ios_base::sync_with_stdio(0);cin.tie(0);
+#define endl '\n'
+#define f first
+#define s second
+
+typedef long long ll;
+
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3fll;
+
+using namespace std;
+
+int main() { _
+
+    int n, r; cin >> n >> r;
+    set<int> s;
+
+    for (int i = 1; i <= n; i++) s.insert(i);
+
+    for (int i = 0; i < r; i++) {
+        int num; cin >> num;
+        s.erase(num);
+    }
+    
+    if (n == r) {
+        cout << "*" << endl;
+    } else {
+        for (int x : s) cout << x << " ";
+        cout << endl;
+    }
+
+    return 0;
+}
+
